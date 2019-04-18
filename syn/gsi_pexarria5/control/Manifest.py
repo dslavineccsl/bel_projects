ba94325adc9b06a6ac1456028caeecf5be95c5f1
Dusan Slavinec <dusan.slavinec@cosylab.com>
2019-04-18 18:33:59 +0200

    adding signal tap to quartus project

--- syn/gsi_pexarria5/control/Manifest.py
+++ syn/gsi_pexarria5/control/Manifest.py
@@ -11,6 +11,8 @@ syn_project = "pci_control"
 
 quartus_preflow = "pci_control.tcl"
 
+files = { "local" : [ "stp4_usb_pcie_pcieb_xwb_master.stp" ]},
+
 modules = {
   "local" : [
     "../../../top/gsi_pexarria5/control",
